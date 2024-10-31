4fe855507c2ec5020610b49a61e31b8cef26f824
Yunan Maulana <maulanayunan2@gmail.com>
2024-10-31 18:50:15 +0700

    Membuat sebuah function didalam component SearchBar

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+const SearchBar = () => {
+  return (
+    <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+        <ul>
+          <li>
+            Best
+            <br />
+            Match
+          </li>
+          <li>
+            Highest
+            <br />
+            Rated
+          </li>
+          <li>
+            Most
+            <br />
+            Reviewed
+          </li>
+        </ul>
+      </div>
+      <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+        <button>Let's Go</button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
