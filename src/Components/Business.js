60d5083f09cc3f8c505810b3dfedfcdf2834847c
Yunan Maulana <maulanayunan2@gmail.com>
2024-10-31 20:44:28 +0700

    Membuat function dalam components Business untuk menampilkan List dari Bisnis

---
+++ src/Components/Business.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Business = ({ business }) => {
+  const {
+    imageSrc,
+    name,
+    address,
+    city,
+    state,
+    zipCode,
+    category,
+    rating,
+    reviewcount,
+  } = business;
+  return (
+    <div className="Business">
+      <div className="Image-container">
+        <img src={imageSrc} alt={name} />
+      </div>
+      <h2 className="Business-name">{name}</h2>
+      <div className="Business-information">
+        <div className="Business-address">
+          <p>{address}</p>
+          <p>{city}</p>
+          <p>{`${state} ${zipCode}`}</p>
+        </div>
+        <div className="Business-reviews">
+          <h3>{category}</h3>
+          <h3 className="rating">{`Rating ${rating}`}</h3>
+          <p>{`${reviewcount} Ulasan`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Business;
