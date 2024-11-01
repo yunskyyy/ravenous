1a320b48778a1f2f0a6cf91a1386fd6212c85f6d
Yunan Maulana <maulanayunan2@gmail.com>
2024-11-01 08:10:54 +0700

    pemberian nilai dalam component businessList sebgai daftar bisnis dan perubahan beberapa kata yang typo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import BusinessList, { businesses } from "./Components/BusinessList";
+import SearchBar from "./Components/SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <SearchBar/>
+      </header>
+      <BusinessList businesses={businesses}/>
     </div>
   );
 }
--- src/Components/BusinessList.js
+++ src/Components/BusinessList.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Business from "./Business";
 
-export const Businesses =[
-    {
-        imageSrc:"",
-        name:"",
-        address:"",
-        city:"",
-        state:"",
-        zipCode:"",
-        category:"",
-        rating:"",
-        reviewCount:"",
-    },
+export const businesses = [
+  {
+    imageSrc: "../Images/ayam_geprek.jpg",
+    name: "AYAM GEPREK",
+    address: "Santiong",
+    city: "Ternate",
+    state: "Indonesia",
+    zipCode: "8667",
+    category: "Makanan",
+    rating: "7,8",
+    reviewCount: "794",
+  },
 ];
 
 const BusinessList = () => {
-    return(
+  return (
     <div className="BusinessList">
-        {Businesses.map((business, index)=>(
-    <Business key={index} bussines={business}/>
-))}
-</div>
-    );
+      {businesses.map((business, index) => (
+        <Business key={index} business={business} />
+      ))}
+    </div>
+  );
 };
 
 export default BusinessList;
